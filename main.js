911506f9f13f944a7135fb66cf59dd8fe35bc09b
FedericoTrimarco <trimarcofederico98@gmail.com>
2021-11-05 15:39:06 +0100

    add function genFeed

--- main.js
+++ main.js
@@ -68,3 +68,49 @@ const containerPostList = document.querySelector('.posts-list');
 
 //-2- generate cards post
 genFeed(feedSocial, containerPostList);
+
+
+
+/***************************
+ F U N Z I O N I
+ ***************************/ 
+
+// function generate cards post
+
+function genFeed(feedSocial, containerPostList){
+    for(let i = 0; i < feedSocial.length; i++){
+        const socialFeed = feedSocial[i];
+        containerPostList.innerHTML += `
+            <div class="post">
+                <div class="post__header">
+                    <div class="post-meta">                    
+                        <div class="post-meta__icon">
+                            <img class="profile-pic" src="${socialFeed.profilePic}" alt="${socialFeed.nameAuthor}">                    
+                        </div>
+                        <div class="post-meta__data">
+                            <div class="post-meta__author">${socialFeed.nameAuthor}</div>
+                            <div class="post-meta__time">${socialFeed.date}</div>
+                        </div>                    
+                    </div>
+                </div>
+                <div class="post__text">${socialFeed.textPost}</div>
+                <div class="post__image">
+                    <img src="${socialFeed.image}" alt="">
+                </div>
+                <div class="post__footer">
+                    <div class="likes js-likes">
+                        <div class="likes__cta">
+                            <a class="like-button  js-like-button" href="#" data-postid="1">
+                                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                <span class="like-button__label">Mi Piace</span>
+                            </a>
+                        </div>
+                        <div class="likes__counter">
+                            Piace a <b id="like-counter-1" class="js-likes-counter">${socialFeed.likes}</b> persone
+                        </div>
+                    </div> 
+                </div>            
+            </div>`
+    };
+
+};
