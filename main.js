7b68e4eef2f2e8b4508a6b8b53ed9e53ef507c5f
FedericoTrimarco <trimarcofederico98@gmail.com>
2021-11-05 15:38:22 +0100

    creat array feedSocial

--- main.js
+++ main.js
@@ -7,3 +7,64 @@
     -2- Prendendo come riferimento il layout di esempio presente nell’html, stampiamo i post del nostro feed.
     Inserire a inizio del file JavaScript una sezione di commento dove scrivere gli step richiesti di logica in italiano, per aiutarvi a ragionare prima della stesura del codice.
  */
+    
+//-1- array 5 objects
+const feedSocial = [
+    // 1
+        {
+            nameAuthor : 'Phil Mangione',
+            profilePic : 'https://picsum.photos/id/237/300/300',
+            image : 'https://picsum.photos/id/238/600/350',
+            date : '4 mesi fa',
+            textPost : 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+            likes : 40,
+
+        },
+    // 2
+        {
+            nameAuthor : 'Federico Trimarco',
+            profilePic : 'https://picsum.photos/id/238/300/300',
+            image : 'https://picsum.photos/id/239/600/350',
+            date : '5 mesi fa',
+            textPost : 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias. bla bla bla',
+            likes : 80,
+
+        },
+    // 3
+        {
+            nameAuthor : 'Francesco Salvati',
+            profilePic : 'https://picsum.photos/id/239/300/300',
+            image : 'https://picsum.photos/id/240/600/350',
+            date : '7 mesi fa',
+            textPost : 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias. bla bla bla bla bla bla bla',
+            likes : 120,
+
+        },
+    // 4
+        {
+            nameAuthor : 'Giulia Ascione',
+            profilePic : 'https://picsum.photos/id/240/300/300',
+            image : 'https://picsum.photos/id/241/600/350',
+            date : '2 mesi fa',
+            textPost : 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias. bla bla',
+            likes : 20,
+
+        },
+    // 5
+        {
+            nameAuthor : 'Fabrizia Abet',
+            profilePic : 'https://picsum.photos/id/250/300/300',
+            image : 'https://picsum.photos/id/242/600/350',
+            date : '11 mesi fa',
+            textPost : 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias. bla bla bla bla bla bla bla bla bla bla',
+            likes : 60,
+
+        },
+
+];
+
+// reference container
+const containerPostList = document.querySelector('.posts-list');
+
+//-2- generate cards post
+genFeed(feedSocial, containerPostList);
